01419aa2276e58f93699f9c8f531a9c1aaa49646
ThanunS <nunsme@gmail.com>
2024-02-19 11:33:28 +0700

    frist typo

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import styles from "./PageNAv.module.css";
+import styles from "./PageNav.module.css";
 import Logo from "./Logo";
 
 function PageNav() {
