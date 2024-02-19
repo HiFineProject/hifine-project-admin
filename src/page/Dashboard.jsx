fcb252f77bc6803f6ef3ecfef7e8c63415a23500
ThanunS <nunsme@gmail.com>
2024-02-19 19:01:33 +0700

    bi

--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -1,5 +1,18 @@
-function Dashborad() {
-  return <div>This is dashboard.jsx.</div>;
+// import React from "react";
+
+function Dashboard() {
+  return (
+    <div>
+      <iframe
+        title="activity50"
+        width="1600"
+        height="800"
+        src="https://app.powerbi.com/reportEmbed?reportId=ef58975a-bfb9-4bc1-b3cb-89f86c92bd7a&autoAuth=true&ctid=2073d876-aa94-436f-8bd7-0e547556def0"
+        frameBorder="0"
+        allowFullScreen="true"
+      ></iframe>
+    </div>
+  );
 }
 
-export default Dashborad;
+export default Dashboard;
