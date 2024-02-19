780c4152331a0da5d2c8b11564a6d7f28b7a9d3a
ThanunS <nunsme@gmail.com>
2024-02-19 09:16:39 +0700

    'UpdatefrontendadminBigdeployV2'

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,13 @@
 import AppNav from "./AppNav";
 import Logo from "./Logo";
 
-// import styles from "./Sidebar.module.css";
-
 function Sidebar() {
   return (
     <aside className="bg-slate-50 p-4 border-r-4 grid row-span-12 row-start-1 auto-rows-min">
       <Logo />
       <AppNav />
 
-      {/* <p>List of Menu</p> */}
-
-      {/* <footer className={styles.footer}> */}
       <footer>
-        {/* <p className={styles.copyright}> */}
         <p>&copy; Copyright{new Date().getFullYear()} by HIFINE</p>
       </footer>
     </aside>
--- src/page/AppLayout.jsx
+++ src/page/AppLayout.jsx
@@ -1,23 +1,16 @@
-// import DataTable from "../components/DataTable";
-// import AppNav from "../components/AppNav";
-// import Sidebar from "../components/SideBar";
 import { Outlet } from "react-router-dom";
-// import styles from "./AppLayout.module.css";
+
 import Header from "../components/Header";
 import Sidebar from "../components/SideBar";
 
 function AppLayout() {
   return (
-    // <div className={styles.app}>
     <div className="grid grid-cols-[300px_auto] h-svh">
-      {/* <Sidebar /> */}
-      {/* <DataTable /> */}
       <Header />
       <Sidebar />
       <main className="bg-slate-200 p-4 row-span-11">
         <Outlet />
       </main>
-      {/* <AppNav /> */}
     </div>
   );
 }
